d7ba710ade8da422e02fdc0deccb6dbf2210e894
Lwong01 <43592585+Lwong01@users.noreply.github.com>
2019-02-19 08:51:03 -0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,47 +1,10 @@
-# In this file we define the methods to help filter out candidates
-# This way, we keep these methods separated from other potential parts of the program
+# This is the main entrypoint into the program
+# It requires the other files/gems that it needs
 
-def find(id)
- @candidates.each do |item|
-   if item[:id]==id
-     @result = item
-   end
- end
- @result
-end
+require 'pry'
+require './candidates'
+require './filters'
 
-def experienced?(candidate)
-  candidate[:years_of_experience] >=2
-end
+## Your test code can go here
 
-def qualified_candidates(candidates)
-  @qualifiedcandidates = []
-  candidates.each do |candidate|
-    if experienced?(candidate) && github(candidate) && languages(candidate) && date_applied(candidate) && age(candidate)
-      @qualifiedcandidates.push(candidate)
-    end
-  end
-  @qualifiedcandidates
-end
-
-# More methods will go below
-def github(candidate)
-  candidate[:github_points] >= 100
-end
-
-def languages(candidate)
-  candidate[:languages].include? ("Ruby" || "Python")
-end
-
-def date_applied(candidate)
-  candidate[:date_applied] >= 15.days.ago
-end
-
-def age(candidate)
-  candidate[:age] >= 18
-end
-
-def ordered_by_qualifications(candidates)
-  @result = candidates.sort_by {|candidate| [-candidate[:years_of_experience], -candidate[:github_points]]}
-end
-# More methods will go below
+puts qualified_candidates(@candidates)
